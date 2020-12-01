0e388df6973531024033c6d0e2d907e8830a22df
paulozterra <66433825+paulozterra@users.noreply.github.com>
2020-12-01 10:51:54 -0500

    1

---
+++ App2/app/src/main/java/com/utec/nickname/app/ANUNCIO.java
@@ -0,0 +1,14 @@
+package com.utec.nickname.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ANUNCIO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_n_u_n_c_i_o);
+    }
+}
---
+++ App2/app/src/main/java/com/utec/nickname/app/CREAR.java
@@ -0,0 +1,14 @@
+package com.utec.nickname.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CREAR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_r_e_a_r);
+    }
+}
